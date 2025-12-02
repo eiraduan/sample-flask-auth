67655611660a622d5d6d1a8af63383ad129dfd9c
Saimor Raduan <saimorraduan@gmail.com>
2025-12-02 13:07:38 -0400

    Criando usuário e realizando autenticação

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET-KEY'] = "your_secret_key"
+app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 login_manager = LoginManager()
@@ -12,6 +12,12 @@ db.init_app(app)
 login_manager.init_app(app)
 
 #view_login
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+     return User.query.get(user_id)
+
 @app.route('/login', methods=["POST"])
 def login():
     data = request.json
@@ -21,6 +27,8 @@ def login():
     user = User.query.filter_by(username=username).first()
 
     if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso"})
 
     return jsonify({"message": "Credenciais inválidas"}), 400 
