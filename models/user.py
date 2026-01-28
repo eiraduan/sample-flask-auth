74e09339a8331dd58e845c6bcd117f29a4efb590
Saimor <saimorraduan@gmail.com>
2026-01-28 09:02:46 -0400

    Create database ORM 'flask shell'

--- models/user.py
+++ models/user.py
@@ -1,6 +1,12 @@
-from app import db
+from database import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
+
+    # flask shell
+    # db.create_all()
+    # db.session
+    # db.session.commit()
